a38faf6375fc2931fb2f63521e1be0345c34ae51
Gabriel Mendes <gagmendes93@gmail.com>
2025-02-06 08:46:21 -0300

    Adding Radix Ui Dialog Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderConatiner, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../../assets/Logo.svg';
 
@@ -7,7 +8,19 @@ export function Header() {
     <HeaderConatiner>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transacao</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transacao</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transacao</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderConatiner>
   );
