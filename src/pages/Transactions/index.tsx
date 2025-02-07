cb817b2b99a0b9faea7477733095e93dba66009f
Gabriel Gonçalves Mendes <53135507+GabrielGoncalvesMendes@users.noreply.github.com>
2025-02-07 14:27:22 -0300

    Create formatter date and price. (#13)

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,6 +8,7 @@ import {
   TransactionsTable 
 } from "./styles";
 import { TransactionContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionContext)
@@ -25,10 +26,13 @@ export function Transactions() {
                   <tr>
                     <td width="50%">{transaction.description}</td>
                     <td>
-                      <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                      <PriceHighlight variant={transaction.type}>
+                        {transaction.type === 'outcome' && '- '}
+                        {priceFormatter.format(transaction.price)}
+                      </PriceHighlight>
                     </td>
                     <td>{transaction.category}</td>
-                    <td>{transaction.createdAt}</td>
+                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                   </tr>
                 );
               })
