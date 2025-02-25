3f598675f70f5e600cdd9daaa7f4ada3db1a8391
Gabriel Gonçalves Mendes <53135507+GabrielGoncalvesMendes@users.noreply.github.com>
2025-02-25 16:38:16 -0300

    Adding useMemo (#23)

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { TransactionContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
 
@@ -6,21 +7,25 @@ export function useSummary() {
     return context.transactions;
   });
 
-  return transactions.reduce(
-    (accumulator, transaction) => {
-      if(transaction.type === 'income') {
-        accumulator.income += transaction.price;
-        accumulator.total += transaction.price;
-      } else {
-        accumulator.outcome += transaction.price;
-        accumulator.total -= transaction.price;
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, transaction) => {
+        if(transaction.type === 'income') {
+          accumulator.income += transaction.price;
+          accumulator.total += transaction.price;
+        } else {
+          accumulator.outcome += transaction.price;
+          accumulator.total -= transaction.price;
+        }
+        return accumulator;
+      }, 
+      { 
+        income: 0, 
+        outcome: 0, 
+        total: 0 
       }
-      return accumulator;
-    }, 
-    { 
-      income: 0, 
-      outcome: 0, 
-      total: 0 
-    }
-  );
+    );
+  }, [transactions]);
+
+  return summary;
 }
